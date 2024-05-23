40b7b81333a1043f10da2cdfeab2279de57fd7df
Caelen Wall <caelen@Caelens-MacBook-Air-2.local>
2024-05-23 13:06:48 -0400

    Adding Pack Class

---
+++ Pack.java
@@ -0,0 +1,18 @@
+public class Pack {
+    
+    private String name;
+    private int price;
+    private ArrayList<Card> cards;
+    private int numberOfRares;
+    
+    public String openPack(String cards){
+
+    }
+
+    public String getCardList(){
+
+    }
+    public boolean buyPack(int bPack){
+
+    }
+}
