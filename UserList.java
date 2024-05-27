893337c578cfca2ffb50ce9c2ac4a9cd92959550
CSVrajTPatel <vpatel9073@outlook.com>
2024-05-27 15:35:20 -0400

    Adding UList

--- UserList.java
+++ UserList.java
@@ -1,17 +1,33 @@
 import java.util.ArrayList;
-
 public class UserList {
     public ArrayList<User> userList;
 
-    public void getUserList() {
+    public UserList() {
+        userList = new ArrayList<>();
+    }
 
+    public ArrayList<User> getUserList() {
+        userList = DataLoader.loadUsers();
+        return userList;
     }
 
     public boolean addUserToList(User user) {
+        if (user != null && !userList.contains(user)) {
+            userList.add(user);
+            DataWriter.updateUsers(userList);
+            return true;
+        }
         return false;
     }
 
-    public boolean removeUserFromList(User user) {
+    public boolean removeUserFromList(String username) {
+        for (User user : userList) {
+            if (user.getUserName().equals(username)) {
+                userList.remove(user);
+                DataWriter.updateUsers(userList);
+                return true;
+            }
+        }
         return false;
     }
-}
+}   
