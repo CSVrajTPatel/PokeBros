7c93c2432f86ff7c353dd0700fe654bf5b73d84e
Caelen Wall <caelen@Caelens-MacBook-Air-2.local>
2024-05-29 13:30:55 -0400

    Fixing and adding to trade class

--- Trade.java
+++ Trade.java
@@ -1,105 +1,86 @@
-import java.util.ArrayList;
-import java.lang.Math;
+import java.util.*;
+
 
 public class Trade {
-    private String buyerUserName;
-    private String sellerUserName;
-    private ArrayList<Integer> cardsOffered;
-    private ArrayList<Integer> cardsRequested;
+    private User sender;
+    private User receiver ;
+    private ArrayList<Card> sendersCards;
+    private Card reciversCard;
     private boolean isFairTrade;
     private boolean awaitingResponse;
     private boolean wasAccepted;
     private String comment;
 
-    public Trade(String buyerUserName, String sellerUserName, ArrayList<Integer> cardsOffered, ArrayList<Integer> cardsRequested, boolean isFairTrade, boolean awaitingResponse, boolean wasAccepted, String comment) {
-        // VP Constructor to initialize Trade object
-        this.buyerUserName = buyerUserName;
-        this.sellerUserName = sellerUserName;
-        this.cardsOffered = cardsOffered;
-        this.cardsRequested = cardsRequested;
+    public Trade(User sender, User seller, ArrayList <Card> sendersCards, Card reciversCard, boolean isFairTrade, boolean awaitingResponse, boolean wasAccepted, String comment){
+        this.sender = sender;
+        this.receiver = seller;
+        this.sendersCards = sendersCards;
+        this.reciversCard = reciversCard;
         this.isFairTrade = isFairTrade;
         this.awaitingResponse = awaitingResponse;
         this.wasAccepted = wasAccepted;
         this.comment = comment;
     }
-
-    // VP Getters and setters for Trade attributes
-    public String getBuyerUserName() {
-        return buyerUserName;
+ 
+    public User getSender() {
+        return sender;
     }
-
-    public void setBuyerUserName(String buyerUserName) {
-        this.buyerUserName = buyerUserName;
-    }
-
-    public String getSellerUserName() {
-        return sellerUserName;
-    }
-
-    public void setSellerUserName(String sellerUserName) {
-        this.sellerUserName = sellerUserName;
-    }
-
-    public ArrayList<Integer> getCardsOffered() {
-        return cardsOffered;
-    }
-
-    public void setCardsOffered(ArrayList<Integer> cardsOffered) {
-        this.cardsOffered = cardsOffered;
-    }
-
-    public ArrayList<Integer> getCardsRequested() {
-        return cardsRequested;
-    }
-
-    public void setCardsRequested(ArrayList<Integer> cardsRequested) {
-        this.cardsRequested = cardsRequested;
+    
+    public User getSeller() {
+        return seller;
     }
 
     public boolean isFairTrade() {
-      CardList masterCardList = new CardList();
-      double buyerCardValue = 0;
-      double sellerCardValue = 0;
-      for(int i = 0; i < cardsOffered.size(); i++){
-        buyerCardValue += masterCardList.searchById(cardsOffered.get(i)).getValue();
+        CardList masterCardList = new CardList();
+        double buyerCardValue = 0;
+        double sellerCardValue = 0;
+        for(int i = 0; i < cardsOffered.size(); i++){
+          buyerCardValue += masterCardList.searchById(cardsOffered.get(i)).getValue();
+        }
+        for(int i = 0; i < cardsRequested.size(); i++){
+          sellerCardValue += masterCardList.searchById(cardsRequested.get(i)).getValue();
+        }
+        // if the cumulative value of the cards is within 50, the trade is fair
+        // otherwise the trade is false
+        if ((Math.abs((sellerCardValue - buyerCardValue)) <=50 )){
+          isFairTrade = true;
+        }
+        return isFairTrade;
+        
       }
-      for(int i = 0; i < cardsRequested.size(); i++){
-        sellerCardValue += masterCardList.searchById(cardsRequested.get(i)).getValue();
-      }
-      // if the cumulative value of the cards is within 50, the trade is fair
-      // otherwise the trade is false
-      if ((Math.abs((sellerCardValue - buyerCardValue)) <=50 )){
-        isFairTrade = true;
-      }
-      return isFairTrade;
-      
-    }
-
-    public void setFairTrade(boolean fairTrade) {
-        isFairTrade = fairTrade;
-    }
 
     public boolean isAwaitingResponse() {
         return awaitingResponse;
     }
-
+   
     public void setAwaitingResponse(boolean awaitingResponse) {
         this.awaitingResponse = awaitingResponse;
     }
+   
+    public void acceptTrade() {
+        this.wasAccepted = true;
+        sender.addCardToList(reciversCard);
+        receiver.removeCardFromList(reciversCard);
 
-    public boolean wasAccepted() {
-        return wasAccepted;
+        for(Card card : sendersCards){
+            receiver.addCardToList(card);
+            sender.removeCardFromList(card);
+        }
     }
 
-    public void setWasAccepted(boolean wasAccepted) {
-        this.wasAccepted = wasAccepted;
+    public void rejectTrade(){
+        this.wasAccepted = false;
     }
 
     public String getComment() {
         return comment;
     }
 
-    public void setComment(String comment) {
-        this.comment = comment;
+    public boolean wasAccepted() {
+        return wasAccepted;
     }
+   
+
+   
+   
 }
