41307f9a6532adbd808178a29f3d34eb02030ba5
Caelen Wall <caelen@Caelens-MacBook-Air-2.local>
2024-06-05 13:35:27 -0400

    test

--- PackTester.java
+++ PackTester.java
@@ -1,5 +1,6 @@
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 import java.util.ArrayList;
 
@@ -11,18 +12,8 @@ public class PackTester {
     public void setUp() {
         masterCardList = CardList.getInstance();
         pack = new Pack(1);
-    }
-/* 
-    public void testForOutOFRangePack() {
-        ArrayList<Card> openedPack = pack.openPack();
-        assertEqual(7, openedPack.size());
-        Pack invalidPack = new Pack(5); // There are only 3 packs, so 5 is out of range
-       assertEquals(7, openedPack.size());
-
-        asserNull()
-    }
-*/
-    
+   }
+  
     @Test
     public void testOpenPack() {
         ArrayList<Card> openedPack = pack.openPack();
@@ -37,11 +28,6 @@ public class PackTester {
         ArrayList<Card> cardList = pack.getCardList();
         assertNotNull(cardList); 
         assertEquals(masterCardList.searchByPack(1), cardList); 
-    }
-
-    @Test
-    public void testBuyPack() {
-        assertTrue(pack.buyPack(1)); 
     }
 
 }
